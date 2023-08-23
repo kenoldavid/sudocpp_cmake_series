cdd946460a38b23ab53c55419e57c48d71d12980
Kenol David <kenoldavid.git@gmail.com>
2023-08-23 04:11:54 -0400

    Using SDL2 - Opening a window

---
+++ Ep04-3rdPartyLibs/main.cpp
@@ -0,0 +1,40 @@
+// Using SDL2: Opening a Window
+// Programming in SDL2 (17 Part Series)
+// https://dev.to/noah11012/using-sdl2-opening-a-window-79c
+
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main(int argv, char** args)
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window by Kenol David",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+}
